d16fb7fe6fe1662ed2efac3ddf1569f732b72225
aumiaik7 <aumi.aik7@gmail.com>
2015-11-23 16:34:20 -0700

    Done 1

--- fgradf.m
+++ fgradf.m
@@ -3,17 +3,18 @@ function vec = fgradf(a0,v0,h0)
 %   [ f(a,b,c), df/dx(a,b,c), df/dy(a,b,c), df/dz(a,b,c) ]
 %   where f is defined in normal syntax below.
 %   This example is range of a tennis serve.
-#{
-a = valder(a0,[1 0 0]); %angle in degrees
-v = valder(v0,[0 1 0]); %velocity in ft/sec
-h = valder(h0,[0 0 1]); %height in ft
+
+a = valder(a0); %angle in degrees
+v = valder(v0); %velocity in ft/sec
+h = valder(h0); %height in ft
 rad = a*pi/180;
 tana = tan(rad);
 vhor = (v*cos(rad))^2;
 f = (vhor/32)*(tana + sqrt(tana^2+64*h/vhor)); %horizontal range
-vec = double(f);
+#vec = double(f);
+mat = single(f);
 #}
-
+#{
 x = valder(a0); 
 y = valder(v0); 
 z = valder(h0); 
@@ -21,9 +22,31 @@ v1 = x*z;
 v2 = x*y;
 v3 = sin(v2);
 v4 = v1*v3;
-vec = single(v4);
+mat = single(v4)
+#}
 
+[m,n] = size(mat);
+new = zeros(m,m);
 
-[m,n] = size(vec);
-new = zeros(n,n);
+  for i=1:m
+	
+    if mat(i,1) ~= 0	 
+	j = int16(mat(i,3));
+    	new(i,j) = mat(i,1);
+    end
+    if mat(i,2) ~= 0
+	j = int16(mat(i,4));	 
+    	new(i,j) = mat(i,2);
+    end 
+  end
+new = new - eye(m);
+derivatives = zeros(1,m);
+derivatives(1,m) = 1
+  for j=m-1:-1:1
+    for i=m:-1:j+1
+	
+	derivatives(1,j) = derivatives(1,j) + derivatives(1,i)*new(i,j);
+    end
+  end
 
+derivatives
--- valder.m
+++ valder.m
@@ -4,110 +4,94 @@ classdef valder
    % starting with a known simple valder such as x=valder(3,1) and 
    % propagating it through elementary functions and operators. 
    % by Richard D. Neidinger 10/23/08
-
-
+	
    properties
       val  %function value
-      der %derivative value or gradient vector
-      pos
+      der  %derivative value or gradient vector
+      pos  %variable position in forward accumulation
       
-      
-           
    end 
    methods
 
       function obj = valder(a,b)
-	   global store = [];
-	   sprintf('**  valder   **');
-          
-	  	
+          sprintf('**  valder   **')
+	 
+   	  global store = [];
+	
+	%global ar
          %VALDER class constructor; only the bottom case is needed.
          if nargin == 0 %never intended for use.
             obj.val = [];
-            #obj.der = [];
-	    #obj.store = [];
+     
    
          elseif nargin == 1 %c=valder(a) for constant w/ derivative 0.
             obj.val = a;
 	    [row,col] = size(store);
-	    store = [store;0 0 row 0];
-	    obj.pos = row;
-	   # obj.der = store;
-            #obj.der = 0;
-	  # obj.store = 0;
-  
+	    store = [store; 0 0 row 0]; %stores the value and position for forward accumulation
+	    obj.pos = row + 1; % position
+	    
          else
-	  
-            obj.val = a; %given function value
+	    obj.val = a; %given function value
             [row,col] = size(store);
-	    store = [store;b];
-	    obj.pos = row;
-	    #obj.der = store;	
-	    #obj.store = c;
- 
-         end
+	    store = [store;b]; %stores the value and position for forward accumulation
+	    obj.pos = row + 1;% position
+	 end
       end
-      
-          
+
       function mat = single(obj)
-		
 	global store;
 	mat = store;
-      end
-
+      end	
       function vec = double(obj)
          %VALDER/DOUBLE Convert valder object to vector of doubles.
-	
-         sprintf('**  double   **')
-         vec = [ obj.der];
-
+         sprintf('**  double   **') ; 
+         vec = [ obj.val obj.der ]
       end
       function h = plus(u,v)
-          sprintf('**  plus   **');
+          sprintf('**  plus   **')
          %VALDER/PLUS overloads addition + with at least one valder object argument
-         #{
-	 if ~isa(u,'valder') %u is a scalar
-            #h = valder(u+v.val, v.der);
-            h = valder(u+v.val, 1);
+         if ~isa(u,'valder') %u is a scalar
+            %h = valder(u+v.val, v.der)
+	    h = valder(u+v.val, [0 1 0 v.pos]);
          elseif ~isa(v,'valder') %v is a scalar
-            #h = valder(u.val+v, u.der);
-            h = valder(u.val+v, 1);
+            % h = valder(u.val+v, u.der)
+	    h = valder(u.val+v, [1 0 u.pos 0]);
          else
-	#}
-            #h = valder(u.val+v.val, u.der+v.der);
-            h = valder(u.val+v.val, 2);
-        # end
+            %h = valder(u.val+v.val, u.der+v.der)
+	    h = valder(u.val+v.val, [1 1 u.pos v.pos]);
+         end
       end
       function h = uminus(u)
           sprintf('**  uminus   **');
          %VALDER/UMINUS overloads negation - with a valder object argument
-         #h = valder(-u.val, -u.der);
-         h = valder(-u.val, -1);
+        % h = valder(-u.val, -u.der)
+	   h = valder(-u.val, [-1 0 u.pos 0]);
       end
       function h = minus(u,v)
           sprintf('**  minus   **');
          %VALDER/MINUS overloads subtraction - with at least one valder object argument
          if ~isa(u,'valder') %u is a scalar
-            #h = valder(u-v.val, -v.der);
-            h = valder(u-v.val, -1);
+            %h = valder(u-v.val, -v.der)
+	    h = valder(u-v.val, [0 -1 0 v.pos]);
          elseif ~isa(v,'valder') %v is a scalar
-            #h = valder(u.val-v, u.der);
-            h = valder(u.val-v, 1);
+           % h = valder(u.val-v, u.der)
+	    h = valder(u.val-v, [1 0 u.pos 0]);
          else
-            h = valder(u.val-v.val, 0);
+            %h = valder(u.val-v.val, u.der-v.der)
+		h = valder(u.val-v.val, [1 -1 u.pos -v.pos]);
          end
       end
-      function h = mtimes(u,v)
+     function h = mtimes(u,v)
           sprintf('**  mtimes   **');
 
          %VALDER/MTIMES overloads multiplication * with at least one valder object argument
          if ~isa(u,'valder') %u is a scalar
             #h = valder(u*v.val, u*v.der);
-            h = valder(u*v.val, u, u);
+            h = valder(u*v.val, [0 u 0 v.pos]);
              
          elseif ~isa(v,'valder') %v is a scalar
             #h = valder(v*u.val, v*u.der);
-            h = valder(v*u.val, v ,v );
+            h = valder(v*u.val, [v 0 u.pos 0]);
          else
 	   
             #h = valder(u.val*v.val, u.der*v.val + u.val*v.der);
@@ -119,25 +103,25 @@ classdef valder
          %VALDER/MRDIVIDE overloads division / with at least one valder object argument
          sprintf('**  mdivide   **');
          if ~isa(u,'valder') %u is a scalar
-            #h = valder(u/v.val, (-u*v.der)/(v.val)^2);
-            h = valder(u/v.val, (-u)/(v.val)^2);
+            %h = valder(u/v.val, (-u*v.der)/(v.val)^2)
+	    h = valder(u/v.val, [ 0 (-u)/(v.val)^2 0 v.pos]);
          elseif ~isa(v,'valder') %v is a scalar
-            #h = valder(u.val/v, u.der/v);
-            h = valder(u.val/v, 1/v);
+            %h = valder(u.val/v, u.der/v)
+	    h = valder(u.val/v, [1/v 0 u.pos 0]);
          else
-            #h = valder(u.val/v.val, (u.der*v.val-u.val*v.der)/(v.val)^2);
-            h = valder(u.val/v.val, (v.val-u.val)/(v.val)^2);
+            %h = valder(u.val/v.val, (u.der*v.val-u.val*v.der)/(v.val)^2)
+            h = valder(u.val/v.val, [1/v.val (-u.val)/(v.val)^2 u.pos v.pos]);
          end
       end
       function h = mpower(u,v)
           sprintf('**  mpower   **');
          %VALDER/MPOWER overloads power ^ with at least one valder object argument
          if ~isa(u,'valder') %u is a scalar
-            #h = valder(u^v.val, u^v.val*log(u)*v.der);
-            h = valder(u^v.val, u^v.val*log(u));
+            %h = valder(u^v.val, u^v.val*log(u)*v.der)
+	    h = valder(u^v.val, [0 u^v.val*log(u) 0 v.pos]);
          elseif ~isa(v,'valder') %v is a scalar
-            #h = valder(u.val^v, v*u.val^(v-1)*u.der);
-            h = valder(u.val^v, v*u.val^(v-1));
+            %h = valder(u.val^v, v*u.val^(v-1)*u.der)
+	    h = valder(u.val^v, [v*u.val^(v-1) 0 u.pos 0]);
          else
             h = exp(v*log(u)); %call overloaded log, * and exp
          end
@@ -145,50 +129,50 @@ classdef valder
       function h = exp(u)
           sprintf('**  exp   **');
          %VALDER/EXP overloads exp of a valder object argument
-         #h = valder(exp(u.val), exp(u.val)*u.der);
-         h = valder(exp(u.val), exp(u.val));
+         % h = valder(exp(u.val), exp(u.val)*u.der)
+	 h = valder(exp(u.val), [exp(u.val) 0 u.pos 0]);
       end
       function h = log(u)
           sprintf('**  log   **');
          %VALDER/LOG overloads natural logarithm of a valder object argument
-         #h = valder(log(u.val), (1/u.val)*u.der);
-         h = valder(log(u.val), (1/u.val));
+         %h = valder(log(u.val), (1/u.val)*u.der)
+ 	 h = valder(log(u.val), [1/u.val 0 u.pos 0]);
       end
       function h = sqrt(u)
           sprintf('**  sqrt   **');
          %VALDER/SQRT overloads square root of a valder object argument
-         #h = valder(sqrt(u.val), u.der/(2*sqrt(u.val)));
-         h = valder(sqrt(u.val), 1/(2*sqrt(u.val)));
+         %h = valder(sqrt(u.val), u.der/(2*sqrt(u.val)))
+	 h = valder(sqrt(u.val), [1/(2*sqrt(u.val)) 0 u.pos 0]);
       end
       function h = sin(u)
           sprintf('**  sin   **');
          %VALDER/SIN overloads sine with a valder object argument
-         #h = valder(sin(u.val), cos(u.val)*u.der);
-         h = valder(sin(u.val), [cos(u.val) 0 u.pos 0]);
+         %h = valder(sin(u.val), cos(u.val)*u.der)
+	h = valder(sin(u.val), [cos(u.val) 0 u.pos 0]);
       end
       function h = cos(u)
           sprintf('**  cos   **');
          %VALDER/COS overloads cosine of a valder object argument
-         #h = valder(cos(u.val), -sin(u.val)*u.der);
-         h = valder(cos(u.val), [u.der; -sin(u.val) 0]);
+         %h = valder(cos(u.val), -sin(u.val)*u.der)
+         h = valder(cos(u.val), [-sin(u.val) 0 u.pos 0]);
       end
       function h = tan(u)
           sprintf('**  tan   **');
          %VALDER/TAN overloads tangent of a valder object argument
-         #h = valder(tan(u.val), (sec(u.val))^2*u.der);
-         h = valder(tan(u.val), (sec(u.val))^2);val
+         %h = valder(tan(u.val), (sec(u.val))^2*u.der)
+	h = valder(tan(u.val), [(sec(u.val))^2 0 u.pos 0]);
       end
       function h = asin(u)
           sprintf('**  asin   **');
          %VALDER/ASIN overloads arcsine of a valder object argument
-         #h = valder(asin(u.val), u.der/sqrt(1-u.val^2));
-         h = valder(asin(u.val), 1/sqrt(1-u.val^2));
+         %h = valder(asin(u.val), u.der/sqrt(1-u.val^2))
+         h = valder(asin(u.val), [1/sqrt(1-u.val^2) 0 u.pos 0]);
       end
       function h = atan(u)
           sprintf('**  atan   **');
          %VALDER/ATAN overloads arctangent of a valder object argument
-         #h = valder(atan(u.val), u.der/(1+u.val^2));
-         h = valder(atan(u.val), 1/(1+u.val^2));
+         %h = valder(atan(u.val), u.der/(1+u.val^2))
+         h = valder(atan(u.val), [1/(1+u.val^2) 0 u.pos 0]);
       end
    end
 end
